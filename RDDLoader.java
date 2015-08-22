16480d2a8f342d80faa0b31c294192a58c19c110
kaniska <kaniska.mandal@gmail.com>
2015-08-21 21:44:30 -0700

    Create RDDLoader.java

---
+++ RDDLoader.java
@@ -0,0 +1,26 @@
+
+
+
+
+    public void createRDD(String strValue, int rowNum) {
+    JavaSparkContext jsc = SparkDriver.getInstance().getSparkContext();
+  	JavaRDD<String> rawDataRDD = jsc.textFile(// load file for a tenant , entity , dataset);
+    
+    // say productId 123 has card 9
+    JavaPairRDD<Integer, Integer> cardinalityRDD = rawDataRDD.map(new PairFunction<String, Integer, Integer>() {
+			private static final long serialVersionUID = -6954711200688980584L;
+
+			@Override
+			public Tuple2<Integer, Integer> call(String line) throws Exception {
+	        	String[] arr = line.split(",");
+		        return new Tuple2<Integer, Integer>(Integer.parseInt(strArr[0]), Double.parseDouble(strArr[1]));
+	        }
+	    });
+    }
+    
+    for(Tuple2<Integer,Double> tuple : cardinalityRDD.collect()) {
+			records.add(tuple);
+		}
+    
+    // say productID 123 occurs in rowList [2,4,5,7,8,9]
+    Tuple2<Integer, List<Integer>> tuple = new Tuple2<Integer, List<Integer>>(Integer.parseInt(strArr[0]), rowList);
